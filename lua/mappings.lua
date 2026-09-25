f3c07daf4233e3f0f23d9129650ac803ecb660ec
nbuttazz <buttazzoni.nicolas@gmail.com>
2026-09-25 09:12:54 +0200

    Orden y arreglo total

--- lua/mappings.lua
+++
@@ -1,9 +0,0 @@
--- add yours here
-
-local map = vim.keymap.set
-
-map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jj", "<ESC>", { desc = "Escape con jj" })
-map("t", "<Esc>", [[<C-\><C-n>]], { desc = "Salir del modo terminal" })
-map("t", "jj", [[<C-\><C-n>]], { desc = "Salir del modo terminal con jj" })
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
